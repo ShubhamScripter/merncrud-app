e870ea23bc672cccd0c111186659772c61b5d33f
s2b4o7 <20243@iiitu.ac.in>
2024-03-27 22:37:02 +0530

    databse successfully created

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
 const app=express();
 const port=3000;
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+dotenv.config();
+
+
+console.log(process.env.mongourl);
+mongoose.connect(process.env.mongourl).then(()=>{console.log("database is connected")}).catch((err)=>{console.log(err)});
 
 app.listen(port,(err)=>{
 
